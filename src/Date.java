79fe570bec34d6182b4324fd3465f4b832d2b27e
Audrey-Costa <audreycosta@nano.ufrj.br>
2023-01-13 14:43:14 -0300

    Adds Date Class

---
+++ src/Date.java
@@ -0,0 +1,17 @@
+public class Date {
+  String dateDay;
+  String holyday;
+
+  public Date(String dateDay, String holiday){
+    this.dateDay = dateDay;
+    this.holyday = holiday;
+  }
+
+  public String getDateDay() {
+    return dateDay;
+  }
+
+  public String getHolyday() {
+    return holyday;
+  }
+}
