4ecb870b98db6d417128b7ecea6248bd9167b99f
Audrey-Costa <audreycosta@nano.ufrj.br>
2023-01-13 14:33:27 -0300

    Adds Date Entry

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Insira uma data:");
+        String date = reader.next();
+        System.out.println(date);
+    }
+}
